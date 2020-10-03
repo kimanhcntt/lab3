cae7cc89792d0ecf080c7de4266a5642887b3227
kimanhCNTT <kimanhcongnghethongtin2000@gmail.com>
2020-10-03 20:19:52 +0700

    first commit

---
+++ Test Suites/TSLAB3.ts
@@ -0,0 +1,74 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TSLAB3</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>5f6e2a59-5f9a-46cb-bbee-824a4b89d9c2</testSuiteGuid>
+   <testCaseLink>
+      <guid>78a84268-aab7-427a-8b7a-4ffe060baa15</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC01</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>d6771533-a146-4f3e-a563-afc91c8aecc2</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>091cbec6-4712-4914-b775-f783b5e2c8db</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC02</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>fee198fc-049f-488d-8579-850e0f6ed5b6</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>7e796405-c7c4-4fab-9cb0-6370c0180596</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC03</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>cfc234c6-b4ff-45e2-88c3-5de21bbedab9</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>4e4e244e-83f4-4ff4-bdab-297e13b2b8b4</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC04</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>4037e222-0d0f-4ac1-8bca-3872310acb51</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>574cbbdb-c2bb-4a06-990b-515dbfa73753</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC05</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>140c3ff2-a9ad-40b8-8cc1-fdf6de096d5d</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
