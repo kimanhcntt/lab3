78ff04870dcf872797ba4f7d300c46e5acbdedbf
kimanhCNTT <kimanhcongnghethongtin2000@gmail.com>
2020-10-10 19:49:03 +0700

    lab4_taoprofiles

--- Test Suites/TSLAB3.ts
+++ Test Suites/TSLAB3.ts
@@ -22,6 +22,12 @@
          <value></value>
          <variableId>d6771533-a146-4f3e-a563-afc91c8aecc2</variableId>
       </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>3323f25d-a9c0-48ae-98d9-760c56f99187</variableId>
+      </variableLink>
    </testCaseLink>
    <testCaseLink>
       <guid>091cbec6-4712-4914-b775-f783b5e2c8db</guid>
@@ -33,6 +39,12 @@
          <type>DEFAULT</type>
          <value></value>
          <variableId>fee198fc-049f-488d-8579-850e0f6ed5b6</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>468ace31-bebe-473f-b2c2-a5fdae92fffa</variableId>
       </variableLink>
    </testCaseLink>
    <testCaseLink>
@@ -46,6 +58,12 @@
          <value></value>
          <variableId>cfc234c6-b4ff-45e2-88c3-5de21bbedab9</variableId>
       </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>f0d7ca82-a76c-4408-9b9c-e9de753fcf59</variableId>
+      </variableLink>
    </testCaseLink>
    <testCaseLink>
       <guid>4e4e244e-83f4-4ff4-bdab-297e13b2b8b4</guid>
@@ -58,6 +76,12 @@
          <value></value>
          <variableId>4037e222-0d0f-4ac1-8bca-3872310acb51</variableId>
       </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>d34b88a7-e79f-45e6-aa26-ff4b5b7b93f1</variableId>
+      </variableLink>
    </testCaseLink>
    <testCaseLink>
       <guid>574cbbdb-c2bb-4a06-990b-515dbfa73753</guid>
@@ -69,6 +93,12 @@
          <type>DEFAULT</type>
          <value></value>
          <variableId>140c3ff2-a9ad-40b8-8cc1-fdf6de096d5d</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>ca4ee4e6-dfbc-4bb7-a674-a7c9a2309cab</variableId>
       </variableLink>
    </testCaseLink>
 </TestSuiteEntity>
